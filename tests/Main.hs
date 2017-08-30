571ced16d806bffb26cb8fee1750551d9352ce0a
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-08-31 01:12:10 +0300

    Update the tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -26,8 +26,6 @@ main =
     C.produceAndConsume (E.list [1,5,2,3]) (D.transform (A.mapFilter (\x -> if x < 5 then Just x else Nothing)) D.list)
     ,
     transformArrowLaws
-    ,
-    transformArrowChoiceLaws
   ]
 
 transformArrowLaws =
@@ -46,66 +44,47 @@ transformArrowLaws =
       (arr (first f))
     ,
     transformProperty "first (f >>> g) = first f >>> first g"
-      (first (arr f >>> arr g) :: A.Transform (Int, Char) (Int, Char))
-      (first (arr f) >>> first (arr g))
+      (first (transform1 >>> transform2) :: A.Transform (Int, Char) (Int, Char))
+      (first (transform1) >>> first (transform2))
     ,
     transformProperty "first f >>> arr fst = arr fst >>> f"
-      (first (arr f) >>> arr fst :: A.Transform (Int, Char) Int)
-      (arr fst >>> arr f)
+      (first transform1 >>> arr fst :: A.Transform (Int, Char) Int)
+      (arr fst >>> transform1)
     ,
     transformProperty "first f >>> arr (id *** g) = arr (id *** g) >>> first f"
-      (first (arr f) >>> arr (id *** g))
-      (arr (id *** g) >>> first (arr f))
+      (first transform1 >>> arr (id *** g))
+      (arr (id *** g) >>> first transform1)
     ,
     transformProperty "first (first f) >>> arr assoc = arr assoc >>> first f"
-      (first (first (arr f)) >>> arr assoc :: A.Transform ((Int, Char), Double) (Int, (Char, Double)))
-      (arr assoc >>> first (arr f))
-  ]
-  where
-    f = (+24) :: Int -> Int
-    g = (*3) :: Int -> Int
-    assoc ((a,b),c) = (a,(b,c))
-
-transformArrowChoiceLaws =
-  testGroup "Transform ArrowChoice laws"
-  [
-    testCase "" $ do
-      let
-        list = [Right 'a', Right 'b', Left 0]
-        transform = left (arr f) >>> left (arr g)
-      result <- C.produceAndConsume (E.list list) (D.transform transform D.list)
-      assertEqual "" [Right 'a', Right 'b', Left 72] result
-    ,
-    testCase "" $ do
-      let
-        list = [Right 'a', Right 'b', Left 0]
-        transform = left (arr f >>> arr g)
-      result <- C.produceAndConsume (E.list list) (D.transform transform D.list)
-      assertEqual "" [Right 'a', Right 'b', Left 72] result
+      (first (first transform1) >>> arr assoc :: A.Transform ((Int, Char), Double) (Int, (Char, Double)))
+      (arr assoc >>> first transform1)
     ,
     transformProperty "left (arr f) = arr (left f)"
       (left (arr f) :: A.Transform (Either Int Char) (Either Int Char))
       (arr (left f))
     ,
     transformProperty "left (f >>> g) = left f >>> left g"
-      (left (arr f >>> arr g) :: A.Transform (Either Int Char) (Either Int Char))
-      (left (arr f) >>> left (arr g))
+      (left (transform1 >>> transform2) :: A.Transform (Either Int Char) (Either Int Char))
+      (left (transform1) >>> left (transform2))
     ,
     transformProperty "f >>> arr Left = arr Left >>> left f"
-      (arr f >>> arr Left :: A.Transform Int (Either Int Char))
-      (arr Left >>> left (arr f))
+      (transform1 >>> arr Left :: A.Transform Int (Either Int Char))
+      (arr Left >>> left transform1)
     ,
     transformProperty "left f >>> arr (id +++ g) = arr (id +++ g) >>> left f"
-      (left (arr f) >>> arr (id +++ g))
-      (arr (id +++ g) >>> left (arr f))
+      (left transform1 >>> arr (id +++ g))
+      (arr (id +++ g) >>> left transform1)
     ,
     transformProperty "left (left f) >>> arr assocsum = arr assocsum >>> left f"
-      (left (left (arr f)) >>> arr assocsum :: A.Transform (Either (Either Int Char) Double) (Either Int (Either Char Double)))
-      (arr assocsum >>> left (arr f))
+      (left (left transform1) >>> arr assocsum :: A.Transform (Either (Either Int Char) Double) (Either Int (Either Char Double)))
+      (arr assocsum >>> left transform1)
   ]
   where
     f = (+24) :: Int -> Int
     g = (*3) :: Int -> Int
+    transform1 = A.take 3 >>> arr f
+    transform2 = A.take 2 >>> arr g
+    assoc ((a,b),c) = (a,(b,c))
     assocsum (Left (Left x)) = Left x
     assocsum (Left (Right y)) = Right (Left y)
     assocsum (Right z) = Right (Right z)

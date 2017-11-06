25a6002a966b22fe9c93c4c9a42b7f73325e211b
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-11-07 02:11:05 +0300

    Dedicated "produce" and "consume" in IO

--- library/Potoki/IO.hs
+++ library/Potoki/IO.hs
@@ -10,3 +10,11 @@ import qualified Potoki.Core.Fetch as D
 produceAndConsume :: A.Produce input -> B.Consume input output -> IO output
 produceAndConsume (A.Produce produce) (B.Consume consume) =
   produce consume
+
+produce :: A.Produce input -> forall x. IO x -> (input -> IO x) -> IO x
+produce (A.Produce produce) stop emit =
+  produce (\ (D.Fetch fetch) -> fetch stop emit)
+
+consume :: (forall x. IO x -> (input -> IO x) -> IO x) -> B.Consume input output -> IO output
+consume fetch (B.Consume consume) =
+  consume (D.Fetch fetch)

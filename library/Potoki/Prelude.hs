87f4b23df2bcd7e2b18752b28cdb432f664c35dd
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-11-16 20:19:21 +0300

    Update the deps

--- library/Potoki/Prelude.hs
+++ library/Potoki/Prelude.hs
@@ -14,20 +14,11 @@ import Control.Arrow as Exports (first, second)
 -------------------------
 import BasePrelude as Exports hiding (first, second)
 
--- contravariant
--------------------------
-import Data.Functor.Contravariant as Exports
-import Data.Functor.Contravariant.Divisible as Exports
-
 -- profunctors
 -------------------------
 import Data.Profunctor.Unsafe as Exports
 import Data.Profunctor.Choice as Exports
 import Data.Profunctor.Strong as Exports
-
--- kan-extensions
--------------------------
-import Control.Monad.Codensity as Exports
 
 -- text
 -------------------------
@@ -40,6 +31,14 @@ import Data.ByteString as Exports (ByteString)
 -- unordered-containers
 -------------------------
 import Data.HashMap.Strict as Exports (HashMap)
+
+-- vector
+-------------------------
+import Data.Vector as Exports (Vector)
+
+-- hashable
+-------------------------
+import Data.Hashable as Exports (Hashable)
 
 -- bug
 -------------------------
